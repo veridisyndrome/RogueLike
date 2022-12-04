337ccc2f2c7704e5ea1040a77371651e4d38a4a5
Pblorg <pablo.robin@epfl.ch>
2022-12-04 13:59:00 +0100

    2.2.3 DONE

---
+++ src/main/java/ch/epfl/cs107/play/game/icrogue/actor/ICRogueActor.java
@@ -0,0 +1,41 @@
+package ch.epfl.cs107.play.game.icrogue.actor;
+
+import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.areagame.actor.MovableAreaEntity;
+import ch.epfl.cs107.play.game.areagame.actor.Orientation;
+import ch.epfl.cs107.play.game.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.math.DiscreteCoordinates;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class ICRogueActor extends MovableAreaEntity {
+    public ICRogueActor(Area area, Orientation orientation, DiscreteCoordinates position) {
+        super(area, orientation, position);
+    }
+
+
+    @Override
+    public List<DiscreteCoordinates> getCurrentCells() {
+        return Collections.singletonList(getCurrentMainCellCoordinates());
+    }
+
+    @Override
+    public boolean takeCellSpace() {
+        return false;
+    }
+
+    @Override
+    public boolean isCellInteractable() {
+        return true;
+    }
+
+    @Override
+    public boolean isViewInteractable() {
+        return true;
+    }
+
+    @Override
+    public void acceptInteraction(AreaInteractionVisitor v, boolean isCellInteraction) {
+
+    }
+}
